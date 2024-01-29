f464e3fb4964066bff8de2d228fdb57a3044369f
Naresh Bollikonda <naresh.bollikonda@wipro.com>
2024-01-29 09:31:05 +0530

    send alert

--- messages/read_msg.go
+++ messages/read_msg.go
@@ -3,21 +3,50 @@ package messages
 import (
 	"fmt"
 	"os"
-	"net/http"
+    "net/http"
 	"io/ioutil"
 	"encoding/json"
 	"bytes"
+	"strconv"
 
 	log "github.com/sirupsen/logrus"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 
 )
 
+type AlertData struct {
+	AssetId int `json:"assetId"`
+	EventCode  string    `json:"eventCode"`
+	DeviceName  string    `json:"deviceName"`
+	Value int `json:"value"`
+	Message string    `json:"message"`
+	SensorName string `json:"sensorName"`
+}
 
 func makeMessageHandler() mqtt.MessageHandler {
 	return func(client mqtt.Client, msg mqtt.Message) {
 
 		fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+		intVar, err := strconv.Atoi(string(msg.Payload()))
+		alertData := &AlertData{
+			AssetId: 34,
+			EventCode: "INSULIN_ACTUATOR",
+			DeviceName:  "Patient_Monitor_19524",
+			Value: intVar,
+			Message: "Anomaly observed for Patient_Monitor_19524 - "+string(msg.Payload()) ,
+			SensorName: "glucose",
+		}
+
+		jsonData, err := json.Marshal(alertData)
+		if err != nil {
+				log.Error("Json Marshal...%+v", alertData)
+		}
+
+		res, err := postData("", "", "POST", jsonData)
+		if err != nil {
+				log.Error("Json Marshal...")
+		}
+		log.Info("postData.."+res)
 
 		log.Info("Sending Insulin actuate command...")
 
@@ -27,11 +56,11 @@ func makeMessageHandler() mqtt.MessageHandler {
 		settings["Bool"] = "true"
 		settings["EnableRandomization_Bool"] = "false"
 
-		jsonData, err := json.Marshal(settings)
+		jsonData, err = json.Marshal(settings)
 		if err != nil {
 			log.Error("Json Marshal...")
 		}
-		res, err := sendCommand(device, command, "post", jsonData)
+		res, err = sendCommand(device, command, "post", jsonData)
 		if err != nil {
 			log.Error("sendCommand error...%v", err)
 		}
@@ -60,6 +89,26 @@ func sendCommand(deviceName string, commandName string, method string, jsonData 
 	}
 
 	return string(respBody), nil
+}
+
+func postData(deviceName string, commandName string, method string, jsonData []byte) (string, error) {
+
+	log.Info("Sending live data...")
+	url := "http://10.239.80.228:8085/api/alerts/createAppAlert"
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Fatalf("Error occurred during http request: %s", err.Error())
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading response body: %s", err.Error())
+	}
+
+	return string(body), nil
 }
 
 /*
